2ba5fc2692ada0f5667eb7e793e5c96e8bafbd60
johanne <johanne@ornsovej.dk>
2025-12-04 18:02:49 +0100

    BlueUnity V2

---
+++ Dist/StudyBuddyBuild1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	14212,
+	g_Il2CppGenericTypes,
+	9904,
+	g_Il2CppGenericInstTable,
+	125317,
+	g_Il2CppGenericMethodFunctions,
+	40374,
+	g_Il2CppTypeTable,
+	146229,
+	g_Il2CppMethodSpecTable,
+	10762,
+	g_FieldOffsetTable,
+	10762,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
